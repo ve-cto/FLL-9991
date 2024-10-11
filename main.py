b9cf4fa1286593e46ae3b18ca8ce6e02adfe9c0c
Vecto <92492351+ve-cto@users.noreply.github.com>
2024-10-11 19:47:29 +0800

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from pybricks.tools import wait, StopWatch, DataLog
 from pybricks.robotics import DriveBase
 from pybricks.media.ev3dev import SoundFile, ImageFile
 
+# SKIBIDI
 
 # This program requires LEGO EV3 MicroPython v2.0 or higher.
 # Click "Open user guide" on the EV3 extension tab for more information.
