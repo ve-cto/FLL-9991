3d58aee8c00710ced8ccc71c8b08b1b62c71d322
ve-cto <lucas.fletcher2@student.education.wa.edu.au>
2024-10-11 19:46:19 +0800

    Gyro turns work now yippee :3

--- main.py
+++ main.py
@@ -12,12 +12,15 @@ from pybricks.media.ev3dev import SoundFile, ImageFile
 # Click "Open user guide" on the EV3 extension tab for more information.
 
 
-# Create your objects here.
+# define the EV3 as ev3 (duh).
 ev3 = EV3Brick()
+
 
 # define the attachment motors
 motor_a = Motor(Port.A)
 motor_d = Motor(Port.D)
+# put the two attachment motors in an array for easier control
+attachment_motors = [motor_a, motor_d]
 
 # define the drive motors
 left_motor = Motor(Port.B)
@@ -30,19 +33,20 @@ robot = DriveBase(left_motor, right_motor, wheel_diameter=60, axle_track=115)
 robot.settings(500, 200, 90, 200)
 
 
-
-
-# set the gyro to 1
+# identify the gyro sensor on port 1
 gyro_sensor = GyroSensor(Port.S1)
 gyro_sensor.reset_angle(0)
 
 
-# put the two attachment motors in an array
-attachment_motors = [motor_a, motor_d]
+# identify the rangefinder on port 2
+# distance_sensor = UltrasonicSensor(Port.S2)
 
 
 
 # --------------------------------------------------------------------------------------------------------------------------------------------------------------------
+# --------------------------------------------------------------------------------------------------------------------------------------------------------------------
+# --------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
 
 def GYRO_STRAIGHT(speed):
     straight_angle = gyro_sensor.angle()
@@ -50,51 +54,60 @@ def GYRO_STRAIGHT(speed):
     while True:
         robot.drive(speed, straight_angle - gyro_sensor.angle())
 
+# -----------------------------------------------------------------------------------
 
 def GYRO_LEFT(angle):
     # get desired turn angle derived from base gyro angle
     starting_angle = gyro_sensor.angle()    
-    desired_angle = starting_angle - angle
-    # print desired to console for debugging
-    
+    desired_angle = starting_angle + angle
 
     # coarse turn
-    while gyro_sensor.angle() >= desired_angle:
-        robot.drive(0, 80)
-    
+    while gyro_sensor.angle() <= desired_angle:
+        robot.drive(0, -120)
+        print(gyro_sensor.angle())
+
     # fine, slower turn for correction
-    while gyro_sensor.angle() <= desired_angle: 
-        robot.drive(0, -20)
+    while gyro_sensor.angle() >= desired_angle: 
+        robot.drive(0, 20)
+        print(gyro_sensor.angle())
 
     # stop
     robot.stop()
+
+
+# -----------------------------------------------------------------------------------
 
 def GYRO_RIGHT(angle):
     # get desired turn angle derived from base gyro angle
     starting_angle = gyro_sensor.angle()    
-    desired_angle = starting_angle + angle
-    # print desired to console for debugging
+    desired_angle = starting_angle - angle
 
     # coarse turn
-    while gyro_sensor.angle() <= desired_angle:
-        robot.drive(0, -80)
-    
-    # fine, slower turn for correction
-    while gyro_sensor.angle() >= desired_angle: 
-        robot.drive(0, 20)
+    while gyro_sensor.angle() >= desired_angle:
+        robot.drive(0, 120)
+        print(gyro_sensor.angle())
 
+    # fine, slower turn for correction
+    while gyro_sensor.angle() <= desired_angle: 
+        robot.drive(0, -20)
+        print(gyro_sensor.angle())
+        
     # stop
     robot.stop()
 
 
-# Write your program here.
+# -----------------------------------------------------------------------------------
+
+
 ev3.speaker.beep()
 
+GYRO_RIGHT(180)
+print(gyro_sensor.angle())
+wait(500)
+GYRO_LEFT(180)
+print(gyro_sensor.angle())
 
-
-
-GYRO_LEFT(90)
-
-
-
-ev3.speaker.beep(frequency=1000, duration=500)
+ev3.speaker.beep()
+wait(20)
+ev3.speaker.beep()
+# ev3.speaker.beep(frequency=1000, duration=500)
