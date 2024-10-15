f0e5f46bde9f8792f6d447b3235b0ca6efff7366
Vecto <92492351+ve-cto@users.noreply.github.com>
2024-10-15 14:01:46 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -17,17 +17,17 @@ ev3 = EV3Brick()
 stopwatch = StopWatch()
 
 # define the attachment motors
-left_attachment = Motor(Port.A)
-right_attachment = Motor(Port.D)
+left_attachment_motor = Motor(Port.A)
+right_attachment_motor = Motor(Port.D)
 # put the two attachment motors in an array for easier control
 attachment_motors = [left_attachment, right_attachment]
 
 # define the drive motors
-left_motor = Motor(Port.B)
-right_motor = Motor(Port.C)
+left_drive_motor = Motor(Port.B)
+right_drive_motor = Motor(Port.C)
 
 # group left_motor and right_motor into the drive base (robot)
-robot = DriveBase(left_motor, right_motor, wheel_diameter=60, axle_track=115)
+robot = DriveBase(left_drive_motor, right_drive_motor, wheel_diameter=60, axle_track=115)
 
 # default driving settings
 robot.settings(500, 200, 90, 200)
