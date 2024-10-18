b7fb75e29e9ef73ef6e2a9421983d2653fa1d9bd
Vecto <92492351+ve-cto@users.noreply.github.com>
2024-10-18 10:05:26 +0800

    drive settings

--- setup.py
+++ setup.py
@@ -11,31 +11,36 @@ from pybricks.media.ev3dev import SoundFile, ImageFile
 # Click "Open user guide" on the EV3 extension tab for more information.
 
 
-# define the EV3 as ev3 (duh).
+# initialize the ev3
 ev3 = EV3Brick()
 
+# initialize the stopwatch
 stopwatch = StopWatch()
 
 # define the attachment motors
 left_attachment_motor = Motor(Port.A)
 right_attachment_motor = Motor(Port.D)
-# put the two attachment motors in an array for easier control
+# put the two attachment motors in an array for sequencial control
 attachment_motors = [left_attachment_motor, right_attachment_motor]
 
-# define the drive motors
+# define the two drive motors
 left_drive_motor = Motor(Port.B)
 right_drive_motor = Motor(Port.C)
 
-# group left_motor and right_motor into the drive base (robot)
+# group left_motor and right_motor into the drive base (robot) and define specifications
 robot = DriveBase(left_drive_motor, right_drive_motor, wheel_diameter=60, axle_track=115)
 
 robot_brake = [left_drive_motor, right_drive_motor]
 
-# default driving settings
-robot.settings(500, 200, 90, 200)
+# initialize default driving settings
+ROBOT_SPEED = 400
+ROBOT_ACCELERATION = 100
+ROBOT_TURN_SPEED = 90
+ROBOT_TURN_ACCELERATION = 80
+robot.settings(ROBOT_SPEED, ROBOT_ACCELERATION, ROBOT_TURN_SPEED, ROBOT_TURN_ACCELERATION)
 
 
-# identify the gyro sensor on port 3
+# define the gyro sensor on port 3 and reset it's angle
 gyro_sensor = GyroSensor(Port.S3)
 gyro_sensor.reset_angle(0)
 
